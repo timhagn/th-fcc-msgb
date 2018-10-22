ce3f186cc45b2799a7e410b2f1c3ae75fcc154e1
timhagn <mail@timhagn.com>
2018-10-22 20:12:30 +0200

    create tests for & basic board functionality with push & get

--- routes/api.js
+++ routes/api.js
@@ -7,13 +7,195 @@
 */
 
 'use strict';
+// For local development.
+require('dotenv').config();
 
-var expect = require('chai').expect;
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const shortid = require('shortid');
+const bcrypt = require('bcrypt');
+
+// Connect to DB.
+mongoose.connect(
+    process.env.MONGO_URI || process.env.MONGO_LOCAL,
+    { useNewUrlParser: true }
+);
+
+// Anonymous Message Board - Schemas
+// Messages
+const MessageSchema = new Schema({
+  _id: {
+    type: Schema.Types.Mixed,
+    default: shortid.generate
+  },
+  text: { type: String, required: true },
+  delete_password: { type: String, required: true },
+  created_on: { type: Date, default: Date.now },
+  reported: { type: Boolean, default: false },
+});
+// Threads
+const ThreadSchema = new Schema({
+  _id: {
+    type: Schema.Types.Mixed,
+    default: shortid.generate
+  },
+  board: { type: String, required: true },
+  text: { type: String, required: true },
+  delete_password: { type: String, required: true },
+  created_on: { type: Date, default: Date.now },
+  bumped_on: { type: Date, default: Date.now },
+  reported: { type: Boolean, default: false },
+  replies: [ MessageSchema ]
+});
+const Thread = mongoose.model('Thread', ThreadSchema);
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
 
+  // Async function to Create a Thread.
+  function createThread(threadObject) {
+    return new Promise((resolve, reject) => {
+      let threadToSave = new Thread( threadObject );
+      threadToSave.save((err, thread) =>
+          err ? reject(null) : resolve(thread));
+    });
+  }
+
+  // Async function to get Thread by thread_id.
+  function findThreadById(thread_id) {
+    return new Promise((resolve, reject) => {
+      Thread.findById(thread_id)
+          .select('-reported -delete_password')
+          .exec((err, thread) => err ? reject(null) : resolve(thread))
+    });
+  }
+
+  // Async function to get newest 10 Threads on Board.
+  function getNewestThreads(board) {
+    return new Promise((resolve, reject) => {
+      Thread.find({ board: board })
+          .limit(10)
+          .sort({ bumped_on: -1 })
+          .select('-reported -delete_password')
+          .exec((err, thread) => err ? reject(null) : resolve(thread))
+    });
+  }
+
+  // Async function to delete Thread by thread_id.
+  function deleteThreadById(thread_id) {
+    return new Promise((resolve, reject) => {
+      Issue.findOneAndDelete(
+          { thread_id: thread_id },
+          (err, thread) => err ? reject(null) : resolve(thread)
+      );
+    });
+  }
+
+  app.route('/api/threads/:board')
+      // Create new Thread.
+      .post(async function (req, res) {
+        let board = req.params.board;
+        let text = req.body.text;
+        let delete_password = req.body.delete_password;
+        if (board && text && delete_password) {
+          let hash = bcrypt.hashSync(delete_password, 12);
+          let threadObject = {
+            board,
+            text,
+            delete_password: hash
+          };
+          let thread = await createThread(threadObject);
+          if (thread) {
+            res.redirect('/b/' + board);
+          }
+          else {
+            res.send('thread creation failure');
+          }
+        }
+        else {
+          res.send('missing fields')
+        }
+      })
+      // Get newest Threads.
+      .get(async function (req, res) {
+        let board = req.params.board;
+        if (board) {
+          let threads = await getNewestThreads(board);
+          if (threads) {
+            threads.forEach(item => {
+              item._doc.replycount = item.replies.length;
+            });
+            res.json(threads);
+          }
+          else {
+            res.json({});
+          }
+        }
+        else {
+          res.send('board not found')
+        }
+      });
+    
+  app.route('/api/replies/:board/')
+      // Create new Reply.
+      .post(async function (req, res) {
+        let board = req.params.board;
+        let text = req.body.text;
+        let delete_password = req.body.delete_password;
+        let thread_id = req.body.thread_id;
+        if (board && text && delete_password && thread_id) {
+          let thread = await findThreadById(thread_id);
+          if (thread) {
+            let hash = bcrypt.hashSync(delete_password, 12);
+            let msgObject = {
+              board,
+              text,
+              delete_password: hash
+            };
+            thread.replies.push(msgObject);
+            thread.bumped_on = new Date();
+            thread.save((err, data) => {
+              err ? res.send('reply creation failed') :
+                  res.redirect('/b/' + board + '/' + thread_id);
+            });
+          }
+          else {
+            res.send('thread not found');
+          }
+        }
+        else {
+          res.send('missing fields')
+        }
+      })
+      // Get Thread.
+      .get(async function (req, res) {
+        let board = req.params.board;
+        let thread_id = req.query.thread_id;
+        if (board && thread_id) {
+          let thread = await findThreadById(thread_id);
+          if (thread) {
+            thread.replies.forEach(item => {
+              delete item._doc.reported;
+              delete item._doc.delete_password;
+            });
+            res.json(thread);
+          }
+          else {
+            res.json({});
+          }
+        }
+        else {
+          res.send('thread not found on board')
+        }
+      });
+
+
+  app.get('/drop', function(req, res) {
+    //if successful response will be 'complete delete successful'
+    Thread.deleteMany( { } , (err, stock) =>
+        err ?
+            res.send('complete delete error') :
+            res.send('complete delete successful')
+    );
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,23 +6,76 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+chai.use(require('chai-url'));
+const assert = chai.assert;
+const server = require('../server');
+const bcrypt = require('bcrypt');
 
 chai.use(chaiHttp);
+
+let preSavedThreadId;
 
 suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
     
     suite('POST', function() {
-      
+      test('Create Thread required fields filled in', function(done) {
+        chai.request(server)
+            .post('/api/threads/general')
+            .send({
+              text: 'New Thread',
+              delete_password: 'password',
+            })
+            .end(function(err, res){
+              assert.equal(res.status, 200);
+              assert.isArray(res.redirects, 'redirects should be array');
+              assert.equal(res.redirects.length, 1, 'redirects should be array of length 1');
+              done();
+            });
+      });
+
+      test('Missing required fields', function(done) {
+        chai.request(server)
+            .post('/api/threads/general')
+            .send({
+              text: 'New Thread'
+            })
+            .end(function(err, res){
+              assert.equal(res.status, 200);
+              console.log(res.text);
+              assert.equal(res.text, 'missing fields');
+              done();
+            });
+      });
+
+
     });
     
     suite('GET', function() {
-      
+      test('GET newest 10 threads', function(done) {
+        chai.request(server)
+            .get('/api/threads/general')
+            .end(function(err, res){
+              assert.equal(res.status, 200);
+              assert.isArray(res.body, 'response should be an array');
+              assert.isObject(res.body[0], 'first thread should be an object');
+              assert.equal(res.body[0].board, 'general', 'board should be general');
+              assert.property(res.body[0], 'board', 'first thread should have board');
+              assert.property(res.body[0], 'replies', 'first thread should have replies');
+              assert.isArray(res.body[0].replies, 'replies should be an array');
+              assert.property(res.body[0], 'bumped_on', 'first thread should have bumped_on');
+              assert.property(res.body[0], 'created_on', 'first thread should have created_on');
+              assert.property(res.body[0], 'replycount', 'first thread should have replycount');
+              assert.property(res.body[0], 'text', 'first thread should have text');
+              assert.property(res.body[0], '_id', 'first thread should have _id');
+              // Save thread_id for later testing.
+              preSavedThreadId = res.body[0]._id;
+              done();
+            });
+      });
     });
     
     suite('DELETE', function() {
@@ -39,11 +92,73 @@ suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/replies/:board', function() {
     
     suite('POST', function() {
-      
+      test('Create Reply required fields filled in', function(done) {
+        chai.request(server)
+            .post('/api/replies/general/')
+            .send({
+              text: 'New Reply',
+              delete_password: 'password',
+              thread_id: preSavedThreadId
+            })
+            .end(function(err, res){
+              assert.equal(res.status, 200);
+              assert.isArray(res.redirects, 'redirects should be array');
+              assert.equal(res.redirects.length, 1, 'redirects should be array of length 1');
+              done();
+            });
+      });
+
+      test('Missing required fields', function(done) {
+        chai.request(server)
+            .post('/api/replies/general/')
+            .send({
+              text: 'New Reply',
+              thread_id: preSavedThreadId
+            })
+            .end(function(err, res){
+              assert.equal(res.status, 200);
+              console.log(res.text);
+              assert.equal(res.text, 'missing fields');
+              done();
+            });
+      });
+
+      test('Missing required fields', function(done) {
+        chai.request(server)
+            .post('/api/replies/general/')
+            .send({
+              text: 'New Reply',
+              delete_password: 'password',
+              thread_id: 'notexisting'
+            })
+            .end(function(err, res){
+              assert.equal(res.status, 200);
+              console.log(res.text);
+              assert.equal(res.text, 'thread not found');
+              done();
+            });
+      });
     });
-    
+
     suite('GET', function() {
-      
+      test('GET threads', function(done) {
+        chai.request(server)
+            .get('/api/replies/general/')
+            .query({thread_id: preSavedThreadId})
+            .end(function(err, res){
+              assert.equal(res.status, 200);
+              assert.isObject(res.body, 'response should be an object');
+              assert.property(res.body, 'board', 'first thread should have board');
+              assert.equal(res.body.board, 'general', 'board should be general');
+              assert.property(res.body, 'replies', 'first thread should have replies');
+              assert.isArray(res.body.replies, 'replies should be an array');
+              assert.property(res.body, 'bumped_on', 'first thread should have bumped_on');
+              assert.property(res.body, 'created_on', 'first thread should have created_on');
+              assert.property(res.body, 'text', 'first thread should have text');
+              assert.property(res.body, '_id', 'first thread should have _id');
+              done();
+            });
+      });
     });
     
     suite('PUT', function() {
